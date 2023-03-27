6a8624880cc1caac0951806aac7801558f29e5d6
thinhnguyen2704 <thinh.9a4.dt@gmail.com>
2023-03-27 17:07:39 +0700

    Merge Product component with App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
-import Product from './components/Product';
+import Category from './components/Category';
 
+interface Product {
+	_id: string;
+	isActive: boolean;
+	price: string;
+	picture: string;
+	category: string;
+	name: string;
+}
 function App() {
+	const [products, setProducts] = useState<Product[]>([]);
+	const [categories, setCategories] = useState<string[]>([]);
+
+	useEffect(() => {
+		fetch('./products.json')
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				const tempCategories: string[] = [];
+				for (let i = 0; i < data.length; i++) {
+					if (!tempCategories.includes(data[i].category)) {
+						tempCategories.push(data[i].category);
+					}
+				}
+				setCategories(tempCategories);
+
+				setProducts(data);
+			})
+			.catch((err: Error) => {
+				console.log(err.message);
+			});
+	}, []);
+
 	return (
 		<div className='App'>
-			<Product />
+			<div className='flex-container'>
+				{categories?.map((category: string, index: number) => {
+					return (
+						<Category key={index} category={category} products={products} />
+					);
+				})}
+			</div>
 		</div>
 	);
 }
