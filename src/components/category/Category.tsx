139d2460371a753401ff485d67704a9d3c5f8bdc
thinhnguyen2704 <thinh.9a4.dt@gmail.com>
2023-03-29 10:19:28 +0700

    Create Category.scss

--- src/components/category/Category.tsx
+++ src/components/category/Category.tsx
@@ -1,4 +1,5 @@
 import { useMemo, useState } from 'react';
+import './Category.scss';
 
 const Category = ({ category, products }: any) => {
 	const [productNames, setProductNames] = useState<string[]>([]);
