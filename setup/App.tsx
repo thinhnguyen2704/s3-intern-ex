b0d595329b85a85125ca2d124a46542fad3bea93
thinhnguyen2704 <thinh.9a4.dt@gmail.com>
2023-03-30 16:48:29 +0700

    Move scss files into a seperated folder

--- setup/App.tsx
+++ setup/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss';
-import Category from './components/category/Category';
-import useProduct from './hooks/useProduct';
+import Category from '../src/components/category/Category';
+import useProduct from '../src/hooks/useProduct';
 
 function App() {
 	const { categories, products } = useProduct();
