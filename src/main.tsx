9f8813f48bc7d6d9fb05072a61338e8b76104961
thinhnguyen2704 <thinh.9a4.dt@gmail.com>
2023-03-31 11:05:22 +0700

    Modify imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../setup/App';
-import '../setup/index.scss';
+import App from './setup/App';
+import './setup/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
