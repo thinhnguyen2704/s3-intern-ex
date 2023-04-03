b61646e250c8b6570c403960e78b510178c52feb
thinhnguyen2704 <thinh.9a4.dt@gmail.com>
2023-04-03 13:11:25 +0700

    Display product deailts

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import { useMemo } from 'react'
+import { useParams } from 'react-router'
+import useProduct from '../hooks/useProduct'
 
 const ProductDetail = () => {
+  const { id } = useParams()
+  const { products } = useProduct('../../products.json')
+ 
+  const productDetail = useMemo(() => {
+    const productsLength = products.length;
+    for (let i = 0; i < productsLength; i++) {
+      if (products[i]._id === id)
+        return products[i]
+    }
+  }, [])
+
   return (
-    <div>ProductDetail</div>
+    <div>
+      {productDetail && (
+        <div>
+          <div>
+            Id: {productDetail._id}
+          </div>
+          <div>
+            Name: {productDetail.name}
+          </div>
+          <div>
+            Category: {productDetail.category}
+          </div>
+          <div>
+            Availability: {productDetail.isActive ? 'Yes' : 'No'}
+          </div>
+          <div>
+            Price: {productDetail.price}
+          </div>
+          <img src={productDetail.picture} alt={productDetail.name} />
+        </div>
+      )}
+    </div>
   )
 }
 
