7bdfee94f84d90cbcea779870dcece225c318503
thinhnguyen2704 <thinh.9a4.dt@gmail.com>
2023-04-03 09:30:23 +0700

    Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import './App.scss'
-import Category from './components/category/Category'
-import useProduct from './hooks/useProduct'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import ProductDetail from './pages/ProductDetail'
 
 function App() {
-  const { categories, products } = useProduct()
 
   return (
     <div className="App">
-      <div className="flex-container">
-        {categories?.map((category: string, index: number) => {
-          return <Category key={index} category={category} products={products} />
-        })}
-      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='product/:id' element={<ProductDetail />} />
+      </Routes>
     </div>
   )
 }
