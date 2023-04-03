9c07171f9b4ca5fe8db477de6bc89293d9170727
thinhnguyen2704 <thinh.9a4.dt@gmail.com>
2023-04-03 09:31:18 +0700

    Create file

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Category from '../components/category/Category'
+import useProduct from '../hooks/useProduct'
+
+const Home = () => {
+  const { categories, products } = useProduct()
+
+  return (
+
+    <div className="flex-container">
+      {categories?.map((category: string, index: number) => {
+        return <Category key={index} category={category} products={products} />
+      })}
+    </div>
+  )
+}
+
+export default Home
