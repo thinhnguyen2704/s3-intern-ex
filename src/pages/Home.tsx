9410ac9029eb294a835ee8130d9b20d7a49bf2bb
thinhnguyen2704 <thinh.9a4.dt@gmail.com>
2023-04-03 13:20:18 +0700

    Remove unused import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
 import Category from '../components/category/Category'
 import useProduct from '../hooks/useProduct'
 
 const Home = () => {
-  const { categories, products } = useProduct()
+  const { categories, products } = useProduct('./products.json')
 
   return (
-
     <div className="flex-container">
       {categories?.map((category: string, index: number) => {
         return <Category key={index} category={category} products={products} />
